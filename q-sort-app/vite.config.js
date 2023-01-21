b419c1b942fdc887b9ee5952b39b4db3e16ffef2
Michal Janů <janum.michal@gmail.com>
2023-01-21 19:14:25 +0100

    added base

--- q-sort-app/vite.config.js
+++ q-sort-app/vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: process.env.NODE_ENV === "production" ? "/Q-SortApp/" : "/",
 	plugins: [vue()],
 	server: {
 		watch:{
