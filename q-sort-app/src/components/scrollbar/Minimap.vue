52115f11f67e77034386d234a55a4583b3714c5a
Michal Janů <janum.michal@gmail.com>
2023-03-05 15:33:36 +0100

    minimap interactable on mobiles

--- q-sort-app/src/components/scrollbar/Minimap.vue
+++ q-sort-app/src/components/scrollbar/Minimap.vue
@@ -1,16 +1,23 @@
 <template>
     <div ref="track" class="mm-track" 
-        @mousedown.left="onMouseDownTrack($event)" >
+        @mousedown.left="onMouseDownTrack($event)" 
+        @touchstart="onMouseDownTrack($event)"
+        @touchend="onMouseUpThumb()"
+        @touchcancel="onMouseUpThumb()">
         <div ref="thumb" class="mm-thumb" 
             @mousedown.left="onMouseDownThumb($event)" 
+            @touchstart.prevent="onMouseDownThumb($event)"
             @mouseup.left="onMouseUpThumb()" 
+            @touchend="onMouseUpThumb()"
+            @touchcancel="onMouseUpThumb()"
             @mousemove="onMoveThumb($event)" 
+            @touchmove="onMoveThumb($event)"
             @mouseleave.left="onMouseUpThumb()">
 
         </div>
         <div class="wrapper">
-            <div ref="panel_fill" class="panel-fill" :style="q_store.getColorStyle(0)">
-            </div>
+            <!-- <div ref="panel_fill" class="panel-fill" :style="q_store.getColorStyle(0)">
+            </div> -->
             <div class="tiles">
                 <Tile v-for="(arr, index) in q_store.table" 
                     :key="index" 
@@ -23,7 +30,7 @@
 </template>
 
 <script setup>
-    import { ref, onMounted, onUnmounted } from 'vue';
+    import { ref, onMounted, onUnmounted, nextTick } from 'vue';
     import { useGlobalStore } from '../../stores/global';
     import { useQSortStore } from '../../stores/q-sort';
     import { useSettingsStore } from '../../stores/settings';
@@ -35,16 +42,23 @@
 
     const thumb = ref()
     const track = ref()
-    const panel_fill = ref()
-
-    const page_height = ref()
-    const track_height = ref()
-    const display_height = ref()
-    const thumb_height = ref()
     
-    const track_click = ref(false)
+    const page_height = ref()
+    const display_height = ref()
+    const row_height = ref()
+
+    const track_height = ref()
+    const thumb_height = ref()
+    const fill_height = ref()
+    const tile_height = ref()
     const thumb_offset = ref(0)
-    const thumb_pos = ref(0)
+    const tile_count = ref()
+
+    const prev_scroll_pos = ref(0)
+    const scroll_pos = ref(0)
+    
+
+    const track_click = ref(false)
     const pressed = ref(false)
 
     defineExpose({init})
@@ -56,17 +70,29 @@
         moveThumb(event)
     }
 
+    function getPosFromEvent(event){
+        var pos
+        if(event.type == 'touchstart' || event.type == 'touchmove' || event.type == 'touchend'){
+            var evt = (typeof event.originalEvent === 'undefined') ? event : event.originalEvent;
+            var touch = evt.touches[0] || evt.changedTouches[0];
+            pos = touch.pageY
+        }else{
+            pos = event.clientY
+        }
+        return pos
+    }
+
     function onMouseDownThumb(event){
         event.stopPropagation();
         track_click.value = false
-        thumb_offset.value = event.clientY
+        thumb_offset.value = getPosFromEvent(event)
         pressed.value = true
         moveThumb(event)
     }
 
     function onMouseUpThumb(){
         if(pressed.value){
-            thumb_pos.value = thumb.value.offsetTop
+            prev_scroll_pos.value = scroll_pos.value
             pressed.value = false
             track_click.value = false
         }
@@ -79,44 +105,32 @@
     }
 
     function moveThumb(event){
-        var offset
-        var new_pos
+        var pos = getPosFromEvent(event)
         if(track_click.value){
-            offset = thumb_height.value/2
-            new_pos = event.clientY - offset
+            scroll_pos.value = pos - thumb_height.value/2
         }else{
-            offset = thumb_offset.value
-            new_pos = event.clientY - offset + thumb_pos.value
+            scroll_pos.value = prev_scroll_pos.value + pos - thumb_offset.value
         }
-        setThumbPosY(new_pos)
+        setThumbPosY()
     }
 
-    function setThumbPosY(new_position){
-        if(new_position < 0){
-            new_position = 0
-        }else if ((new_position + thumb_height.value) > display_height.value){
-            new_position = display_height.value - thumb_height.value
+    function setThumbPosY(){
+        if(scroll_pos.value < 0){
+            scroll_pos.value = 0
         }
-        changeThumbOnScroll(new_position)
-        thumb.value.style.top = new_position.toString() + "px"  
+        changeThumbOnScroll()
     }
 
-    function convertPage2Scroll(position){
-        return (position / page_height.value) * track_height.value
-    }
-
-    function convertSctoll2page(position){
-        return (position / track_height.value) * page_height.value
-    }
-
-    function changeThumbOnScroll(new_position){
+    function changeThumbOnScroll(){
         var container = document.getElementById("container")
-        container.scrollTo(0, convertSctoll2page(new_position))
+        container.scrollTo(0, convertSctoll2pageHeight(scroll_pos.value))
+        getScroll()
     }
 
     function getScroll(){
         var container = document.getElementById("container")
-        thumb.value.style.top = convertPage2Scroll(container.scrollTop).toString() + "px"
+        thumb.value.style.top = Math.max(convertPage2ScrollHeight(container.scrollTop) - fill_height.value,0).toString() + "px"
+        updateThumbHeight()
     }
 
     function init(){
@@ -129,18 +143,30 @@
         if(s_store.minimap_enabled){
             var panel = document.getElementById("panel-h")
             var table = document.getElementById("table-h")
-    
-            page_height.value = panel.offsetHeight + table.offsetHeight
-            // page_height.value = table.offsetHeight
+            
+            display_height.value = window.innerHeight
+            page_height.value = table.offsetHeight
+            row_height.value = document.getElementsByClassName("row")[0].offsetHeight
             
             track_height.value = track.value.offsetHeight
-            display_height.value = window.innerHeight
-    
-            thumb_height.value = convertPage2Scroll(display_height.value)
-            thumb.value.style.height = thumb_height.value.toString() + "px"
-    
-            panel_fill.value.style.height = convertPage2Scroll(panel.offsetHeight).toString() + "px"
+            fill_height.value = convertPage2ScrollHeight(panel.offsetHeight)
+            tile_height.value = document.getElementsByClassName("tile")[0].offsetHeight
+            tile_count.value = display_height.value / row_height.value
+            updateThumbHeight()
         }
+    }
+
+    function updateThumbHeight(){
+        thumb_height.value = tile_count.value*tile_height.value - Math.max(fill_height.value - convertPage2ScrollHeight(container.scrollTop), 0)
+        thumb.value.style.height = thumb_height.value.toString() + "px"
+    }
+
+    function convertPage2ScrollHeight(height){
+        return (height / page_height.value) * track_height.value
+    }
+
+    function convertSctoll2pageHeight(height){
+        return (height / track_height.value ) * page_height.value
     }
 
     onMounted(() => {
