482935a4e1e47c19b86021627829eab3f5df02b0
Michal Janů <janum.michal@gmail.com>
2023-03-08 01:28:36 +0100

    added datasets and fix minimap

--- q-sort-app/src/router/index.js
+++ q-sort-app/src/router/index.js
@@ -1,27 +1,33 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Sorting from "../views/Sorting.vue"
+import Home from "../views/Home.vue"
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/Sorting',
-  //   name: 'Sorting',
-  //   component: Sorting
-  // }
-  {
-      path: process.env.NODE_ENV === "production" ? "/Q-SortApp/" : "/",
-      name: 'Sorting',
-      component: Sorting
-    }
+	// {
+	//   path: '/',
+	//   name: 'Home',
+	//   component: Home
+	// },
+	// {
+	//   path: '/Sorting',
+	//   name: 'Sorting',
+	//   component: Sorting
+	// }
+	{
+		path: process.env.NODE_ENV === "production" ? "/Q-SortApp/Sorting" : "/Sorting/:uid",
+		name: "Sorting",
+		component: Sorting
+	},
+	{
+		path: "/",
+		name: "Home",
+		component: Home
+	}
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+	history: createWebHistory(),
+	routes
 })
 
 export default router
---
+++ q-sort-app/src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="wrapper">
+        <div class="q-list" @click="goToRoute('Sorting', {uid: 2})">
+            <div class="title">
+                Q-sorting Datasets
+            </div>
+            <div class="options-wrapper">
+
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+    import { useRouter } from 'vue-router';
+    import { useGlobalStore } from "../stores/global" 
+    const router = useRouter()
+    const g_store = useGlobalStore()
+
+    function goToRoute(route_name, params=null, ){
+        router.push({name: route_name, params: params==null? {} : params})
+    }
+</script>
+<style lang="scss" scoped>
+    @use "../scss/Colors/Colors" as *;
+    .wrapper{
+        background-color: #8CB37F;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .q-list{
+            min-width: 280px;
+            height: fit-content;
+            width: min(80vmin, 600px);
+            background-color: $primary_bg;
+            border-radius: 6px;
+            padding: 20px;
+            box-sizing: border-box;
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            .title{
+                color: #FFFFFF;
+                font-size: max(20pt, min(7vmin, 30pt));
+                font-variation-settings: 'wght' 500;
+            }
+            .options-wrapper{
+                width: 100%;
+                height: 400px;
+                background-color: $secondary_bg;
+                border-radius: 6px;
+            }
+        }
+    }
+</style>
