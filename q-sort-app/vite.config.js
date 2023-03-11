9c8886bd97f45a1b31fca8c55100ffab13259700
Michal Janů <janum.michal@gmail.com>
2023-03-11 12:12:35 +0100

    Add changes to routes

--- q-sort-app/vite.config.js
+++ q-sort-app/vite.config.js
@@ -3,7 +3,8 @@ import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === "production" ? "/Q-SortApp/" : "/",
+	// base: process.env.NODE_ENV === "production" ? "/Q-SortApp/" : "/",
+	base: "/",
 	plugins: [vue()],
 	server: {
 		watch:{
