beea0bc45b1ef227368ba2fb51be373e8a53811f
Michal Janů <janum.michal@gmail.com>
2023-03-16 14:24:11 +0100

    disable dragscroll on mobile

--- q-sort-app/src/views/Sorting.vue
+++ q-sort-app/src/views/Sorting.vue
@@ -3,10 +3,13 @@
     <div class="mm-s-wrapper">
         <div id="container" class="sorting-wrapper"  
             :style="getTopBgColor()" 
-            v-dragscroll.pass
+            v-dragscroll="!is_touch"
             @dragscrollstart="start"
             @dragscrollend="end"
-            @click.capture="click">
+            @click.capture="click"
+            @touchstart="() => {is_touch.value = true}"
+            @touchend="() => {is_touch.value = false}"
+            @touchcancel="() => {is_touch.value = false}">
             <div id="panel-h" class="top-panel-wrapper">
                 <TopPanel />
             </div>
@@ -26,15 +29,15 @@
     import { useQSortStore } from "../stores/q-sort"
     import { useSettingsStore } from "../stores/settings"
     import { useGlobalStore } from "../stores/global"
-    import { ref, onMounted, onBeforeMount, nextTick} from "vue"
-    import { useRoute, useRouter } from "vue-router"
+    import { ref, onMounted, onBeforeMount} from "vue"
+    import { useRoute } from "vue-router"
     
     const q_store = useQSortStore()
     const s_store = useSettingsStore()
     const g_store = useGlobalStore()
     const route = useRoute()
-    const router = useRouter()
-    
+    const is_touch = ref(false)
+
     let dragging = false;
     let timer = null;
 
