7f2d959a2dd323f5f10a88aeed21387371896370
Michal Janů <janum.michal@gmail.com>
2023-04-23 12:56:53 +0200

    removed unused file

--- q-sort-app/vite.config.js
+++ q-sort-app/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	// base: process.env.NODE_ENV === "production" ? "/Q-SortApp/" : "/",
 	base: "/",
 	plugins: [vue()],
 	server: {
